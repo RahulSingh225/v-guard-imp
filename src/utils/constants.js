08e18005b697c4489bf37247d296c625e828941b
Rahul Singh <rahul.singh@xuriti.com>
2023-10-10 17:00:14 +0530

    initial commit

---
+++ src/utils/constants.js
@@ -0,0 +1,26 @@
+export const Colors = {
+  colorPrimary: '#F0C300',
+  colorPrimaryDark: '#F0C300',
+  primary_light: '#f9e799',
+  colorAccent: '#2cb0e1',
+  primary_text: '#212121',
+  text: '#5C5B5B',
+  secondary_text: '#757575',
+  white: '#FFFFFF',
+  black: '#000104',
+  red: '#FF0000',
+  coupon_page: '#7798e9',
+  grey_light: '#BDBDBD',
+  activity_bg_color: '#FFFFFF',
+  yellow: '#F0C300',
+  transparent_white: '#00FFFFFF',
+  custom_black: '#000104',
+  grey: '#9E9E9E',
+  grey_400: '#BDBDBD',
+  login_box_divider: '#B7BDBDBD',
+  blue: '#3F51B5',
+  white_translucent: '#70ffffff',
+};
+
+export const baseUrl = "http://202.66.175.34:18091/vguard/api/";
+export const imageUrl = "https://vguardrishta.com/";
