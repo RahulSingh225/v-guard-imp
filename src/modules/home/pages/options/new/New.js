29cc8412f34f2b0e8cf21a4b0fdcab6f2bc55148
shrutigxuriti <shruti.ghosh@xuriti.com>
2023-10-25 15:27:59 +0530

    what's new and welfare completed

--- src/modules/home/pages/options/new/New.js
+++ src/modules/home/pages/options/new/New.js
@@ -1,12 +1,81 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React from 'react';
+import { View, Text, ScrollView, TouchableOpacity, Linking, StyleSheet } from 'react-native';
+import colors from '../../../../../../colors';
+import { responsiveFontSize } from 'react-native-responsive-dimensions';
 
 const New = () => {
-  return (
-    <View>
-      <Text>New</Text>
-    </View>
-  )
-}
+  const data = [
+    {
+      message: 'Link 1',
+      link: 'https://www.youtube.com/',
+    },
+    {
+      message: 'Link 2',
+      link: 'https://www.youtube.com/',
+    },
+    {
+      message: 'Link 3',
+      link: 'https://www.youtube.com/',
+    },
+    {
+      message: 'Link 4',
+      link: 'https://www.youtube.com/',
+    },
+  ];
 
-export default New
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>What's New?</Text>
+      <ScrollView>
+        {data.map((item, index) => (
+          <TouchableOpacity
+            key={index}
+            style={styles.listItem}
+            onPress={() => Linking.openURL(item.link)}
+          >
+            <View style={styles.messageContainer}>
+              <Text style={styles.messageText}>{item.message}</Text>
+            </View>
+            <Text style={styles.openLinkText}>View</Text>
+          </TouchableOpacity>
+        ))}
+      </ScrollView>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 15,
+    backgroundColor: colors.white
+  },
+  title: {
+    fontSize: responsiveFontSize(2.5),
+    fontWeight: 'bold',
+    color: colors.black,
+    textAlign: 'center',
+    marginBottom: 10,
+  },
+  listItem: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: colors.lightGrey,
+  },
+  messageContainer: {
+    flex: 1,
+  },
+  messageText: {
+    fontSize: responsiveFontSize(2),
+    color: colors.black
+  },
+  openLinkText: {
+    color: colors.yellow,
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+});
+
+export default New;
--- src/modules/home/pages/options/welfare/Welfare.js
+++ src/modules/home/pages/options/welfare/Welfare.js
@@ -1,12 +1,26 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
+import colors from '../../../../../../colors'
 
 const Welfare = () => {
   return (
-    <View>
-      <Text>Welfare</Text>
+    <View style={styles.mainWrapper}>
+      <Text style={styles.text}>No Data</Text>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  text: {
+    color: colors.grey,
+    fontWeight: 'bold'
+  },
+  mainWrapper: {
+    height: '100%',
+    backgroundColor: colors.white,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
 
 export default Welfare
